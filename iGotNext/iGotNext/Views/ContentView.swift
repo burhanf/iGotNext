f078f4772aec51104b58dec45c49b3561a1d5889
burhanf <burhan_faquiri@hotmail.com>
2021-11-21 14:05:07 -0500

    User location now displays.

--- iGotNext/iGotNext/Views/ContentView.swift
+++ iGotNext/iGotNext/Views/ContentView.swift
@@ -2,15 +2,61 @@
 //  ContentView.swift
 //  iGotNext
 //
-//  Created by Burhan Faquiri on 2021-11-21.
+//  Created by Michael Wright on 2021-11-11.
 //
 
 import SwiftUI
 
 struct ContentView: View {
+    @State var loginToggle = false
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        VStack{
+            NavigationView{
+                VStack{
+                    NavigationLink(destination: Login(),isActive: $loginToggle){
+                        Button("Login"){
+                            loginToggle = true // Programatically navigate
+                        }
+                    }
+                    NavigationLink(destination: Signup()){
+                        Text("Signup")
+                    }
+                    NavigationLink(destination: MapView()){
+                        Text("Map")
+                    }
+                    NavigationLink(destination: HomePage()){
+                        Text("Home")
+                    }
+                    
+                    NavigationLink(destination: UserProfile()){
+                        Text("User Profile")
+                    }
+                    
+                    NavigationLink(destination: CreateGameView()){
+                        Text("Create Game")
+                    }
+                    // swift being swift
+                    Group {
+                        NavigationLink(destination: GameInformationView()){
+                            Text("Game Information")
+                        }
+                        
+                        NavigationLink(destination: ReviewsView()){
+                            Text("Reviews")
+                        }
+                        NavigationLink(destination: FriendsView()){
+                            Text("Friends")
+                        }
+                        NavigationLink(destination: HistoryView()){
+                            Text("History")
+                        }
+                        NavigationLink(destination: GameInfoModalView()){
+                            Text("Modal")
+                        }
+                    }
+                }
+            }
+        }
     }
 }
 
