a6c21716f5c6dfeda246a74f778d11131061e0d1
Burhan Faquiri <59700142+burhanf@users.noreply.github.com>
2021-11-21 13:38:47 -0500

    Add files via upload

---
+++ iGotNext/iGotNext WatchKit Extension/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  iGotNext WatchKit Extension
+//
+//  Created by Burhan Faquiri on 2021-11-21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ iGotNext/iGotNext WatchKit Extension/HostingController.swift
@@ -0,0 +1,16 @@
+//
+//  HostingController.swift
+//  iGotNext WatchKit Extension
+//
+//  Created by Burhan Faquiri on 2021-11-21.
+//
+
+import WatchKit
+import Foundation
+import SwiftUI
+
+class HostingController: WKHostingController<ContentView> {
+    override var body: ContentView {
+        return ContentView()
+    }
+}
