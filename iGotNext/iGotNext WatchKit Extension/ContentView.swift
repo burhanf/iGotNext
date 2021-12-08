6542e7b31878dbb6df59e4e2bcc8487254e3ee53
burhanf <59700142+burhanf@users.noreply.github.com>
2021-12-07 20:18:56 -0500

    Updates watch ContentView

--- iGotNext/iGotNext WatchKit Extension/ContentView.swift
+++ iGotNext/iGotNext WatchKit Extension/ContentView.swift
@@ -7,39 +7,22 @@
 
 import SwiftUI
 
-struct GameView : View{
-    var game : Game
-    var body : some View{
-        VStack{
-            //Text("Location: \(game.location?.latitude ?? defaultLocation.latitude) & \(game.location?.longitude ?? defaultLocation.longitude)")
-            Text("Game type: \(game.gameType ?? "No game type")")
-            Text("Start time: \(game.startTime ?? Date())")
-            Text("End time: \(game.endTime ?? Date())")
-            Text("Number of spots available: \(game.numOfPlayers ?? 0) / \(game.maxPlayers ?? 0)")
-        }
-    }
-}
+
 
 
 
 struct ContentView: View {
     let viewModel = WatchGameViewModel(connectivityProvider: ConnectionProvider())
-    @State var games : [Game] = []
     
     var body: some View {
-        VStack{
-            Text("Game from iPhone")
-            
-            List(games, id:\.self ){ g in
-                GameView(game: g)
+        NavigationView{
+            VStack{
+                Text("Watch from iPhone")
+                NavigationLink(destination: WatchGameView(viewModel: viewModel)){
+                    Text("Game from iPhone")
+                }
             }
-
-    } .onAppear(){
-        // Step 5d - initialize connection between watch and phone
-        // Step 5e - request data and deserialize data from phone
-        viewModel.connectivityProvider.connect()
-        self.games = viewModel.connectivityProvider.receivedGames
-}
+        }
     }
 }
 
