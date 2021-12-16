a18578c828dd764bfa1b6d2dd3645b7c6589d994
Amir Azizafshari <55924754+amir002001@users.noreply.github.com>
2021-12-16 16:27:50 -0500

    FriendsView now gets user

--- iGotNext/iGotNext/Views/FriendsView.swift
+++ iGotNext/iGotNext/Views/FriendsView.swift
@@ -9,16 +9,28 @@ import SwiftUI
 import Firebase
 
 struct FriendsView: View {
-    var auth = Auth.auth().currentUser?.uid
+    var userId = Auth.auth().currentUser?.uid
+    private let db = Firestore.firestore()
+    
     var body: some View {
         List {
             
+        }.onAppear{
+            fetchData()
         }
+    }
+    
+    func fetchData(){
+        
+        var user = db.collection("User").document(userId!).getDocument { (document, error) in
+            if let document = document, document.exists {
+                let dataDescription = document.data().map(String.init(describing:)) ?? "nil"
+                print("Document data: \(dataDescription)")
+            } else {
+                print("Document does not exist")
+            }
+        }
+        
     }
 }
 
-struct FriendsView_Previews: PreviewProvider {
-    static var previews: some View {
-        FriendsView()
-    }
-}
